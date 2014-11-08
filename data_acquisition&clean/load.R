06d6b1cb98447b545a8df60f10a2f04821796323
Jovan Sardinha <jovan.sardinha@gmail.com>
2014-11-08 13:18:26 -0500

    Removed Quiz 1 material

--- data_acquisition&clean/load.R
+++ data_acquisition&clean/load.R
@@ -9,23 +9,3 @@ cname <- file.path(".", "data", "en_US")
 docs <-Corpus(DirSource(cname))
 #class(docs)
 
-# File Analysis
-enUSBlogs <- docs[1][[1]][1]
-enUSNews <- docs[2][[1]][1]
-enUSTwitter <- docs[3][[1]][1]
-
-# For en US
-lengthBlogs <- lapply(enUSBlogs$content, function(x) nchar(x))
-maxLengthBlogs <- max(unlist(lengthBlogs))
-
-lengthNews <- lapply(enUSNews$content, function(x) nchar(x))
-maxLengthNews <- max(unlist(lengthNews))
-
-lengthTwitter <- lapply(enUSTwitter$content, function(x) nchar(x))
-maxLengthTwitter <- max(unlist(lengthTwitter))
-
-
-# Analyzing the love/hate relationship
-loveTwitter <- lapply(enUSTwitter$content, function(x) grep("love", x))
-hateTwitter <- lapply(enUSTwitter$content, function(x) grep("hate", x))
-loveHateRel <- sum(unlist(loveTwitter))/sum(unlist(hateTwitter)) 
